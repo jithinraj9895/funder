48b045a0ffc0045c819cf957cb014a7aef9cec26
User <jithinraj9895@gmail.com>
2025-02-18 01:29:12 +0530

    Fixed minor bug

--- src/main/java/com/fts/funder/service/UserDetailsServiceImp.java
+++ src/main/java/com/fts/funder/service/UserDetailsServiceImp.java
@@ -4,7 +4,9 @@ import com.fts.funder.repository.UserRepository;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 
+@Service
 public class UserDetailsServiceImp implements UserDetailsService {
 
     private final UserRepository repository;
