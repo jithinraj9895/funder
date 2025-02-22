b45ad0ea059cc7f96cd7cdef6da9812f894dac3f
User <jithinraj9895@gmail.com>
2025-02-22 18:29:51 +0530

    Added idea controller setup in user controller

--- src/main/java/com/fts/funder/controllers/UserController.java
+++ src/main/java/com/fts/funder/controllers/UserController.java
@@ -1,9 +1,13 @@
 package com.fts.funder.controllers;
 
+import com.fts.funder.model.Idea;
 import com.fts.funder.model.User;
+import com.fts.funder.repository.IdeaRepository;
 import com.fts.funder.repository.UserRepository;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -11,13 +15,31 @@ import java.util.List;
 @RestController
 public class UserController {
     private final UserRepository userRepository;
+    private final IdeaRepository ideaRepository;
 
-    public UserController(UserRepository userRepository) {
+    public UserController(UserRepository userRepository, IdeaRepository ideaRepository) {
         this.userRepository = userRepository;
+        this.ideaRepository = ideaRepository;
     }
 
     @GetMapping("/users")
     public ResponseEntity<List<User>> getUsers(){
         return ResponseEntity.ok(userRepository.findAll());
+    }
+
+    @GetMapping("/ideas")
+    public ResponseEntity<List<Idea>> getIdeas(){
+        return ResponseEntity.ok(ideaRepository.findAll());
+    }
+
+    @PostMapping("/idea")
+    public ResponseEntity<String> setIdea(@RequestBody Idea request){
+        Idea idea = new Idea();
+        idea.setTitle(request.getTitle());
+        idea.setDescription(request.getDescription());
+        idea.setApprovals(request.getApprovals());
+        idea.setDisapprovals(request.getDisapprovals());
+        ideaRepository.save(idea);
+        return ResponseEntity.ok("Idea saved.");
     }
 }
